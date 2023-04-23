4e561c855fc083e1e4a38aa2afec51f650812036
Suresh Khirwadkar <85228274+sureshk999@users.noreply.github.com>
2023-04-23 11:49:51 +1000

    Initial commit

---
+++ components/Tag.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+import kebabCase from '@/lib/utils/kebabCase'
+
+const Tag = ({ text }) => {
+  return (
+    <Link href={`/tags/${kebabCase(text)}`}>
+      <a className="mr-3 text-xl font-medium uppercase text-blue-700 hover:text-primary-600 dark:hover:text-primary-400">
+        {text.split(' ').join('-')}
+      </a>
+    </Link>
+  )
+}
+
+export default Tag
---
+++ pages/_app.js
@@ -0,0 +1,54 @@
+import '@/css/tailwind.css'
+import '@/css/prism.css'
+import 'katex/dist/katex.css'
+
+import '@fontsource/inter/variable-full.css'
+
+import { ThemeProvider } from 'next-themes'
+import Head from 'next/head'
+
+import siteMetadata from '@/data/siteMetadata'
+import Analytics from '@/components/analytics'
+import LayoutWrapper from '@/components/LayoutWrapper'
+import { ClientReload } from '@/components/ClientReload'
+
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+const isDevelopment = process.env.NODE_ENV === 'development'
+const isSocket = process.env.SOCKET
+
+export default function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+  const site = 'https://drsuresh.com.au'
+  const canonicalURL = site + useRouter().asPath
+
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', function () {
+        navigator.serviceWorker.register('/sw.js').then(
+          function (registration) {
+            console.log('Service Worker registration successful with scope: ', registration.scope)
+          },
+          function (err) {
+            console.log('Service Worker registration failed: ', err)
+          }
+        )
+      })
+    }
+  }, [])
+
+  return (
+    <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
+      <Head>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
+      {isDevelopment && isSocket && <ClientReload />}
+      <Analytics />
+      <LayoutWrapper>
+        <Component {...pageProps} />
+      </LayoutWrapper>
+    </ThemeProvider>
+  )
+}
