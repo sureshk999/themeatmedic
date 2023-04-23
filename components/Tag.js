fc9b4b9b190b17040422f219c5059bde042eea82
SURESH KHIRWADKAR <suresh.khirwadkar@gmail.com>
2023-04-23 21:52:29 +1000

    revert post-css

--- components/Tag.js
+++ components/Tag.js
@@ -4,7 +4,7 @@ import kebabCase from '@/lib/utils/kebabCase'
 const Tag = ({ text }) => {
   return (
     <Link href={`/tags/${kebabCase(text)}`}>
-      <a className="mr-3 text-xl font-medium uppercase text-blue-700 hover:text-primary-600 dark:hover:text-primary-400">
+      <a className="mr-3 text-xl font-medium uppercase text-blue-300 hover:text-primary-600 dark:hover:text-primary-400">
         {text.split(' ').join('-')}
       </a>
     </Link>
--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,7 +1,7 @@
 const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
+  { href: 'https://drsuresh.teachable.com', title: 'Courses' },
   { href: '/about', title: 'About' },
 ]
 
