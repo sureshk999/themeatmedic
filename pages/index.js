d4c242660c8622bdc59d38c6d6b468ebf2c9ed91
SURESH KHIRWADKAR <suresh.khirwadkar@gmail.com>
2023-04-23 20:06:38 +1000

    remove other newsletter

--- pages/index.js
+++ pages/index.js
@@ -126,8 +126,6 @@ export default function Home({ posts }) {
       {siteMetadata.newsletter.provider !== '' && (
         <div className="flex items-center justify-center pt-4">
           <NewsletterForm />
-
-          <Newsletter />
         </div>
       )}
     </>
