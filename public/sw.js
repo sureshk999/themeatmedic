e7e28ac0f5dd89d57c8f3b8e9d5817f46fe0d4f4
SURESH KHIRWADKAR <suresh.khirwadkar@gmail.com>
2023-04-23 20:00:48 +1000

    change sw

--- public/sw.js
+++ public/sw.js
@@ -56,23 +56,21 @@ self.addEventListener('activate', (event) => {
 // If no response is found, it populates the runtime cache with the response
 // from the network before returning it to the page.
 self.addEventListener('fetch', (event) => {
-  // Skip cross-origin requests, like those for Google Analytics.
-  if (event.request.url.startsWith(self.location.origin)) {
+  if (event.request.method === 'GET' && event.request.url.startsWith(self.location.origin)) {
+    // handle GET requests as usual
     event.respondWith(
       caches.match(event.request).then((cachedResponse) => {
         if (cachedResponse) {
           return cachedResponse
         }
-
-        return caches.open(RUNTIME).then((cache) => {
-          return fetch(event.request).then((response) => {
-            // Put a copy of the response in the runtime cache.
-            return cache.get(event.request, response.clone()).then(() => {
-              return response
-            })
+        return fetch(event.request).then((response) => {
+          return caches.open(RUNTIME).then((cache) => {
+            cache.put(event.request, response.clone())
+            return response
           })
         })
       })
     )
   }
+  // ignore all other requests, including POST requests
 })
