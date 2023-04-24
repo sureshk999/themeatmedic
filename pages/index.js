0324e07852cec9044f77bc7e06fef4eaa9b17073
SURESH KHIRWADKAR <suresh.khirwadkar@gmail.com>
2023-04-24 20:56:23 +1000

    change texts

--- pages/index.js
+++ pages/index.js
@@ -116,7 +116,7 @@ export default function Home({ posts }) {
         <div className="flex justify-end text-base font-medium leading-6">
           <Link
             href="/blog"
-            className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+            className="text-primary-800 hover:text-primary-600 dark:text-primary-200 dark:hover:text-primary-400"
             aria-label="all posts"
           >
             All Posts &rarr;
