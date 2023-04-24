0f198ddba95bd3f5eaf2c6078b6859061e145e71
SURESH KHIRWADKAR <suresh.khirwadkar@gmail.com>
2023-04-25 07:09:36 +1000

    fix cache

--- next.config.js
+++ next.config.js
@@ -21,8 +21,8 @@ module.exports = withPWA({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self';
-  style-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline';
+  style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   media-src 'none';
   connect-src *;
