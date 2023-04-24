995c58410b3242c830ce06e617b45b4c9862d3b5
SURESH KHIRWADKAR <suresh.khirwadkar@gmail.com>
2023-04-24 21:55:01 +1000

    change button text

--- pages/index.js
+++ pages/index.js
@@ -63,7 +63,7 @@ export default function Home({ posts }) {
               href="https://drsuresh.com.au"
               className="rounded-md bg-black py-2 px-4 text-xl font-bold text-white hover:bg-primary-500 dark:bg-white dark:text-black dark:hover:bg-primary-500 "
             >
-              Click Here
+              Book Now
             </a>
           </div>
         </div>
