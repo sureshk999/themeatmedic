8911b0c416a7fafa942e56a00dea5bedcd7df0ba
SURESH KHIRWADKAR <suresh.khirwadkar@gmail.com>
2023-05-13 20:36:41 +1000

    add social share

--- components/Share.js
+++ components/Share.js
@@ -17,7 +17,7 @@ export default function Text() {
     <div className="flex grid grid-cols-1 flex-wrap place-items-center overflow-hidden px-5 xl:items-start">
       <h1>Share This Article</h1>
       <div className="py-2">
-        <FacebookShareButton url={'http://localhost:3000'}>
+        <FacebookShareButton url={'window.location.href'}>
           <FacebookIcon size={32} round />
         </FacebookShareButton>
         <PinterestShareButton url={'http://localhost:3000'}>
@@ -32,20 +32,6 @@ export default function Text() {
         <LinkedinShareButton url={'http://localhost:3000'}>
           <LinkedinIcon size={32} round />
         </LinkedinShareButton>
-      </div>
-      <div>
-        <a
-          href="https://twitter.com/share?ref_src=twsrc%5Etfw"
-          className="twitter-share-button"
-          data-size="large"
-          data-text="Check out this article!"
-          data-via="themeatmedic"
-          data-hashtags="themeatmedic"
-          data-show-count="false"
-        >
-          Tweet
-        </a>
-        <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
       </div>
     </div>
   )
