556974b83a3cf430efadf39a35d42103e1cf0fa5
SURESH KHIRWADKAR <suresh.khirwadkar@gmail.com>
2023-05-13 20:46:03 +1000

    change social shares

--- components/Share.js
+++ components/Share.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import Head from 'next/head'
 import {
   FacebookShareButton,
   FacebookIcon,
@@ -12,27 +15,21 @@ import {
   LinkedinIcon,
 } from 'next-share'
 
-export default function Text() {
+const SocialShareButtons = () => {
+  const router = useRouter()
+
+  useEffect(() => {
+    const url = window.location.href
+    router.prefetch(url)
+  }, [router])
+
   return (
-    <div className="flex grid grid-cols-1 flex-wrap place-items-center overflow-hidden px-5 xl:items-start">
-      <h1>Share This Article</h1>
-      <div className="py-2">
-        <FacebookShareButton url={'window.location.href'}>
-          <FacebookIcon size={32} round />
-        </FacebookShareButton>
-        <PinterestShareButton url={'http://localhost:3000'}>
-          <PinterestIcon size={32} round />
-        </PinterestShareButton>
-        <RedditShareButton url={'http://localhost:3000'}>
-          <RedditIcon size={32} round />
-        </RedditShareButton>
-        <WhatsappShareButton url={'http://localhost:3000'}>
-          <WhatsappIcon size={32} round />
-        </WhatsappShareButton>
-        <LinkedinShareButton url={'http://localhost:3000'}>
-          <LinkedinIcon size={32} round />
-        </LinkedinShareButton>
-      </div>
-    </div>
+    <>
+      <FacebookShareButton url={typeof window !== 'undefined' && window.location.href}>
+        Share on Facebook
+      </FacebookShareButton>
+    </>
   )
 }
+
+export default SocialShareButtons
