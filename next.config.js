ff12bfefb92fe202c379f8e595a176dbf4c6c96f
SURESH KHIRWADKAR <suresh.khirwadkar@gmail.com>
2023-05-25 21:57:17 +1000

    fix margins

--- next.config.js
+++ next.config.js
@@ -102,23 +102,3 @@ module.exports = withBundleAnalyzer({
     return config
   },
 })
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: '/',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 's-maxage=1, stale-while-revalidate=59',
-          },
-        ],
-      },
-    ]
-  },
-}
-
-module.exports = nextConfig
