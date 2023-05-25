b20a2e6aeebb59967ef74761437a0f1fadb05bab
SURESH KHIRWADKAR <suresh.khirwadkar@gmail.com>
2023-05-25 21:59:50 +1000

    add cache

--- next.config.js
+++ next.config.js
@@ -102,3 +102,23 @@ module.exports = withBundleAnalyzer({
     return config
   },
 })
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/index',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 's-maxage=1, stale-while-revalidate=59',
+          },
+        ],
+      },
+    ]
+  },
+}
+
+module.exports = nextConfig
