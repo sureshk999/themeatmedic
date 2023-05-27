e0eba0300aabd6ca1c48f2497fdc4e677c33613e
SURESH KHIRWADKAR <suresh.khirwadkar@gmail.com>
2023-05-27 18:47:18 +1000

    change client reload

--- components/ClientReload.js
+++ components/ClientReload.js
@@ -4,19 +4,32 @@ import Router from 'next/router'
 /**
  * Client-side complement to next-remote-watch
  * Re-triggers getStaticProps when watched mdx files change
- *
  */
 export const ClientReload = () => {
-  // Exclude socket.io from prod bundle
   useEffect(() => {
-    import('socket.io-client').then((module) => {
-      const socket = module.io()
-      socket.on('reload', (data) => {
-        Router.replace(Router.asPath, undefined, {
-          scroll: false,
+    if (process.env.NODE_ENV === 'production') {
+      const socket = new WebSocket('ws://localhost:3000') // Replace with your WebSocket server URL
+
+      socket.addEventListener('message', (event) => {
+        const data = JSON.parse(event.data)
+
+        if (data.event === 'reload') {
+          Router.replace(Router.asPath, undefined, {
+            scroll: false,
+          })
+        }
+      })
+    } else {
+      import('socket.io-client').then((module) => {
+        const socket = module.io()
+
+        socket.on('reload', (data) => {
+          Router.replace(Router.asPath, undefined, {
+            scroll: false,
+          })
         })
       })
-    })
+    }
   }, [])
 
   return null
