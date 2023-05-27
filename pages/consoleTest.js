b444eaba85b3710c5604d41a5204e31572354244
SURESH KHIRWADKAR <suresh.khirwadkar@gmail.com>
2023-05-27 18:09:56 +1000

    update next

---
+++ pages/consoleTest.js
@@ -0,0 +1,15 @@
+console.log('Console log test')
+
+function addNumbers(a, b) {
+  console.log('Adding numbers:', a, '+', b)
+  return a + b
+}
+
+const result = addNumbers(5, 10)
+console.log('Result:', result)
+
+const person = { name: 'John', age: 25 }
+console.log('Person:', person)
+
+const colors = ['red', 'green', 'blue']
+console.log('Colors:', colors)
