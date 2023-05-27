0190606fcb6b5779764b2305271d6a4347cce39e
SURESH KHIRWADKAR <suresh.khirwadkar@gmail.com>
2023-05-27 20:24:59 +1000

    adjust service worker cache to 60s

--- public/sw.js
+++ public/sw.js
@@ -1,28 +1,10 @@
-/*
- Copyright 2016 Google Inc. All Rights Reserved.
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
- http://www.apache.org/licenses/LICENSE-2.0
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
- */
-
-// Names of the two caches used in this version of the service worker.
-// Change to v2, etc. when you update any of the local resources, which will
-// in turn trigger the install event again.
 const PRECACHE = 'precache-v1'
 const RUNTIME = 'runtime'
 
-// A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
   './', // Alias for index.html
 ]
 
-// The install handler takes care of precaching the resources we always need.
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches
@@ -32,7 +14,6 @@ self.addEventListener('install', (event) => {
   )
 })
 
-// The activate handler takes care of cleaning up old caches.
 self.addEventListener('activate', (event) => {
   const currentCaches = [PRECACHE, RUNTIME]
   event.waitUntil(
@@ -52,25 +33,32 @@ self.addEventListener('activate', (event) => {
   )
 })
 
-// The fetch handler serves responses for same-origin resources from a cache.
-// If no response is found, it populates the runtime cache with the response
-// from the network before returning it to the page.
 self.addEventListener('fetch', (event) => {
   if (event.request.method === 'GET' && event.request.url.startsWith(self.location.origin)) {
-    // handle GET requests as usual
     event.respondWith(
       caches.match(event.request).then((cachedResponse) => {
         if (cachedResponse) {
           return cachedResponse
         }
-        return fetch(event.request).then((response) => {
-          return caches.open(RUNTIME).then((cache) => {
-            cache.put(event.request, response.clone())
+        return fetch(event.request)
+          .then((response) => {
+            // Clone the response since it can only be consumed once
+            const clonedResponse = response.clone()
+
+            // Open the runtime cache and store the response with a 60-second expiration
+            caches.open(RUNTIME).then((cache) => {
+              cache.put(event.request, clonedResponse, { expiration: { maxAgeSeconds: 60 } })
+            })
+
             return response
           })
-        })
+          .catch((error) => {
+            // Handle fetch errors
+            console.error('Error fetching:', error)
+            // You can customize the fallback response here if needed
+          })
       })
     )
   }
-  // ignore all other requests, including POST requests
+  // Ignore all other requests, including POST requests
 })
